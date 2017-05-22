7a5f33665e58f80ea0c71f9a86716f9db48ff44c
Evgen Korcheviy <korcheviy.evgen21@gmail.com>
2017-05-22 10:50:57 +0300

    fix create request method

--- src/test/java/util/RequestCreator.java
+++ src/test/java/util/RequestCreator.java
@@ -12,104 +12,90 @@ public class RequestCreator {
     private static final String baseURL = "http://www.webserviceX.NET/";
     private static final String filePath = "src/test/resources/request.xml";
 
-    private static SOAPMessage createRequest(String requestName) throws SOAPException, FileNotFoundException {
-        MessageFactory messageFactory = MessageFactory.newInstance();
-        SOAPMessage soapMessage = messageFactory.createMessage();
-        SOAPPart soapPart = soapMessage.getSOAPPart();
 
-        soapPart.setContent(new StreamSource(new FileInputStream(filePath)));
-
-        SOAPEnvelope soapEnvelope = soapPart.getEnvelope();
+    public static SOAPMessage getAirportInformationByCountry(String country) throws IOException, SOAPException {
+        MessageFactory factory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = factory.createMessage();
+        SOAPPart part = soapMessage.getSOAPPart();
+        SOAPEnvelope envelope = part.getEnvelope();
         MimeHeaders headers = soapMessage.getMimeHeaders();
-        headers.addHeader("SOAPAction", baseURL + requestName);
-        soapMessage.saveChanges();
+        headers.addHeader("SOAPAction", baseURL + "GetAirportInformationByCountry");
+        SOAPBody body = envelope.getBody();
+
+        SOAPBodyElement element = body.addBodyElement(envelope.createName("GetAirportInformationByCountry", "", "http://www.webserviceX.NET"));
+        element.addChildElement("country").addTextNode(country);
+
+        soapMessage.writeTo(System.out);
+        FileOutputStream fOut = new FileOutputStream(filePath);
         return soapMessage;
     }
 
-    public static SOAPMessage getAirportInformationByCountry(String country) throws IOException, SOAPException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath));
-        StringBuilder builder = new StringBuilder();
-        builder.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
-                "    <soap:Body>\n" +
-                "        <GetAirportInformationByCountry xmlns=\"http://www.webserviceX.NET\">\n" +
-                "            <country>");
-        builder.append(country);
-        builder.append("</country>\n" +
-                "        </GetAirportInformationByCountry>\n" +
-                "    </soap:Body>\n" +
-                "</soap:Envelope>");
-        bufferedWriter.write(builder.toString());
-        bufferedWriter.close();
-        return createRequest("GetAirportInformationByCountry");
-    }
-
     public static SOAPMessage getAirportInformationByAirportCode(String code) throws IOException, SOAPException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath));
-        StringBuilder builder = new StringBuilder();
-        builder.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
-                "  <soap:Body>\n" +
-                "    <getAirportInformationByAirportCode xmlns=\"http://www.webserviceX.NET\">\n" +
-                "      <airportCode>");
-        builder.append(code);
-        builder.append("</airportCode>\n" +
-                "    </getAirportInformationByAirportCode>\n" +
-                "  </soap:Body>\n" +
-                "</soap:Envelope>");
-        bufferedWriter.write(builder.toString());
-        bufferedWriter.close();
-        return createRequest("getAirportInformationByAirportCode");
+        MessageFactory factory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = factory.createMessage();
+        SOAPPart part = soapMessage.getSOAPPart();
+        SOAPEnvelope envelope = part.getEnvelope();
+        MimeHeaders headers = soapMessage.getMimeHeaders();
+        headers.addHeader("SOAPAction", baseURL + "getAirportInformationByAirportCode");
+        SOAPBody body = envelope.getBody();
+
+        SOAPBodyElement element = body.addBodyElement(envelope.createName("getAirportInformationByAirportCode", "", "http://www.webserviceX.NET"));
+        element.addChildElement("airportCode").addTextNode(code);
+
+        soapMessage.writeTo(System.out);
+        FileOutputStream fOut = new FileOutputStream(filePath);
+        return soapMessage;
+
     }
 
     public static SOAPMessage getAirportInformationByCityOrAirportName(String cityOrName) throws IOException, SOAPException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath));
-        StringBuilder builder = new StringBuilder();
-        builder.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
-                "  <soap:Body>\n" +
-                "    <getAirportInformationByCityOrAirportName xmlns=\"http://www.webserviceX.NET\">\n" +
-                "      <cityOrAirportName>");
-        builder.append(cityOrName);
-        builder.append("</cityOrAirportName>\n" +
-                "    </getAirportInformationByCityOrAirportName>\n" +
-                "  </soap:Body>\n" +
-                "</soap:Envelope>");
-        bufferedWriter.write(builder.toString());
-        bufferedWriter.close();
-        return createRequest("getAirportInformationByCityOrAirportName");
+        MessageFactory factory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = factory.createMessage();
+        SOAPPart part = soapMessage.getSOAPPart();
+        SOAPEnvelope envelope = part.getEnvelope();
+        MimeHeaders headers = soapMessage.getMimeHeaders();
+        headers.addHeader("SOAPAction", baseURL + "getAirportInformationByCityOrAirportName");
+        SOAPBody body = envelope.getBody();
+
+        SOAPBodyElement element = body.addBodyElement(envelope.createName("getAirportInformationByCityOrAirportName", "", "http://www.webserviceX.NET"));
+        element.addChildElement("cityOrAirportName").addTextNode(cityOrName);
+
+        soapMessage.writeTo(System.out);
+        FileOutputStream fOut = new FileOutputStream(filePath);
+        return soapMessage;
     }
 
     public static SOAPMessage getAirportInformationByISOCountryCode(String ISO) throws IOException, SOAPException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath));
-        StringBuilder builder = new StringBuilder();
-        builder.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
-                "  <soap:Body>\n" +
-                "    <getAirportInformationByISOCountryCode xmlns=\"http://www.webserviceX.NET\">\n" +
-                "      <CountryAbbrviation>");
-        builder.append(ISO);
-        builder.append("</CountryAbbrviation>\n" +
-                "    </getAirportInformationByISOCountryCode>\n" +
-                "  </soap:Body>\n" +
-                "</soap:Envelope>");
-        bufferedWriter.write(builder.toString());
-        bufferedWriter.close();
-        return createRequest("getAirportInformationByISOCountryCode");
+        MessageFactory factory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = factory.createMessage();
+        SOAPPart part = soapMessage.getSOAPPart();
+        SOAPEnvelope envelope = part.getEnvelope();
+        MimeHeaders headers = soapMessage.getMimeHeaders();
+        headers.addHeader("SOAPAction", baseURL + "getAirportInformationByISOCountryCode");
+        SOAPBody body = envelope.getBody();
+
+        SOAPBodyElement element = body.addBodyElement(envelope.createName("getAirportInformationByISOCountryCode", "", "http://www.webserviceX.NET"));
+        element.addChildElement("CountryAbbrviation").addTextNode(ISO);
+
+        soapMessage.writeTo(System.out);
+        FileOutputStream fOut = new FileOutputStream(filePath);
+        return soapMessage;
     }
 
     public static SOAPMessage createWrongMessage() throws IOException, SOAPException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath));
-        StringBuilder builder = new StringBuilder();
-        builder.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
-                "  <soap:Body>\n" +
-                "    <getAirportInformationByISOCountryCode xmlns=\"http://www.webserviceX.NET\">\n");
-        builder.append("    </getAirportInformationByISOCountryCode>\n" +
-                "  </soap:Body>\n" +
-                "</soap:Envelope>");
-        bufferedWriter.write(builder.toString());
-        bufferedWriter.close();
-        return createRequest("getAirportInformationByISOCountryCode");
+        MessageFactory factory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = factory.createMessage();
+        SOAPPart part = soapMessage.getSOAPPart();
+        SOAPEnvelope envelope = part.getEnvelope();
+        MimeHeaders headers = soapMessage.getMimeHeaders();
+        headers.addHeader("SOAPAction", baseURL + "GetAirportInformationByCountry");
+        SOAPBody body = envelope.getBody();
+
+        SOAPBodyElement element = body.addBodyElement(envelope.createName("GetAirportInformationByCountry", "", "http://www.webserviceX.NET"));
+
+        soapMessage.writeTo(System.out);
+        FileOutputStream fOut = new FileOutputStream(filePath);
+        return soapMessage;
     }
+
 }
